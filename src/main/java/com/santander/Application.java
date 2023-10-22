20fe34c26cd6de12ec2bb0b3a8a89c55e6377762
jhonathanfeijo <jhonathanfeijomoraes@gmail.com>
2023-10-22 15:24:35 -0400

    adicionando applications de dev e prod

--- src/main/java/com/santander/Application.java
+++ src/main/java/com/santander/Application.java
@@ -8,5 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setAdditionalProfiles("prod");
 	}
 }
